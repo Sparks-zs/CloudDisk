c3450ac13f597926f575a75a55e66f28647fbe08
Sparks-zs <572448977@qq.com>
2023-04-11 19:27:48 +0800

    优化

--- config.h
+++ config.h
@@ -6,6 +6,6 @@ const int PORT = 8888;
 const char URL[] = "http://192.168.1.17:8888/clouddisk";
 
 const int UPLOAD_SIZE = 1024 * 1024 * 40; // 40MB
-const int DOWNLOAD_SIZE = 1024 * 1024 * 30;  // 30MB
+const int DOWNLOAD_SIZE = 1024 * 1024 * 40;  // 30MB
 
 #endif // CONFIG_H
